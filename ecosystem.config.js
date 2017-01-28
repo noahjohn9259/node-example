b68d9a438c5d9ec585523b6abdbeb12744b5b656
Noah John Ucab <noahjohn.ucab@gmail.com>
2017-01-28 22:30:19 +0800

    commit files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // First application
     {
       name      : "carcareapp-api",
-      script    : "server/index.js",
+      script    : "app.js",
       env_production : {
         NODE_ENV: "production"
       }
